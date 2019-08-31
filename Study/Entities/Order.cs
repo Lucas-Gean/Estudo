65f7a814a17165d3b7500f7827a9d131af7d8d4e
Lucas <lucas11marques@hotmail.com>
2019-08-31 06:48:14 -0300

    Estudos

---
+++ Study/Entities/Order.cs
@@ -0,0 +1,78 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Study.Entities.Enums;
+using System.Globalization;
+
+namespace Study.Entities
+{
+    class Order
+    {
+        public DateTime Moment { get; set; }
+        public OrderStatus Status { get; set; }
+        public Client Client { get; set; }
+        public List<OrderItem> Items { get; set; } = new List<OrderItem>();
+
+        public Order()
+        {
+
+        }
+
+        public Order(DateTime moment, OrderStatus status, Client client)
+        {
+            Moment = moment;
+            Status = status;
+            Client = client;
+        }
+
+        public void AddItem(OrderItem item)
+        {
+            Items.Add(item);
+        }
+        public void RemoveItem(OrderItem item)
+        {
+            Items.Remove(item);
+        }
+
+        public double Total()
+        {
+            double total = 0;
+            foreach(OrderItem it in Items)
+            {
+                total += it.subTotal();
+            }
+
+            return total;
+        }
+
+        public override string ToString()
+        {
+            StringBuilder sb = new StringBuilder();
+            sb.Append("Order moment: ");
+            sb.AppendLine(Moment.ToString());
+            sb.Append("Order status: ");
+            sb.AppendLine(Status.ToString());
+            sb.Append("Client: ");
+            sb.Append(Client.Name + " ");
+            sb.Append("(" + Client.BirthDate.ToShortDateString() + ")");
+            sb.Append(" - ");
+            sb.AppendLine(Client.Email);    
+            sb.AppendLine("Order items:");
+
+            foreach(OrderItem it in Items)
+            {
+                sb.Append(it.Product.Name + ", " + it.Price.ToString("F2",CultureInfo.InvariantCulture) + " ");
+                sb.Append("Quantity: ");
+                sb.Append(it.Quantity + " ");
+                sb.Append("Subtotal: $");
+                sb.Append(it.subTotal().ToString("F2", CultureInfo.InvariantCulture));
+                sb.AppendLine();
+                    
+            }
+            sb.Append("Total price: $");
+            sb.AppendLine(Total().ToString("F2", CultureInfo.InvariantCulture));
+            return sb.ToString();
+
+        }
+    }
+}
---
+++ Study/Program.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using Study.Entities.Enums;
+using Study.Entities;
+using System.Globalization;
+
+namespace Study
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter cliente data:");
+            Console.Write("Name: ");
+            string name = Console.ReadLine();
+
+            Console.Write("Email: ");
+            string email = Console.ReadLine();
+
+            Console.Write("Birth date (DD/MM/YYYY): ");
+            DateTime birth = DateTime.Parse(Console.ReadLine());
+
+            Console.WriteLine("Enter order data:");
+
+            Console.Write("Status: ");
+            OrderStatus status = Enum.Parse<OrderStatus>(Console.ReadLine());
+
+            Client client = new Client(name, email, birth);
+
+            Order order = new Order(DateTime.Now, status, client);
+
+            Console.Write("How many items ot this order? ");
+            int n = int.Parse(Console.ReadLine());
+
+            for(int i = 1; i <= n; i++)
+            {
+                Console.WriteLine($"Enter #{i} item data");
+                Console.Write("Product name: ");
+                name = Console.ReadLine();
+                Console.Write("Product price: ");
+                double price = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+                Console.Write("Quantity: ");
+                int quantity = int.Parse(Console.ReadLine());
+
+                Product product = new Product(name, price);
+
+                OrderItem item = new OrderItem(quantity, price, product);
+
+                order.AddItem(item);
+                
+
+            }
+
+            Console.WriteLine(order);
+
+
+
+
+        }
+    }
+}
